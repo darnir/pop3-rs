c61a29329936eb66e6e50c6a1fada849f24fa006
Darshit Shah <darnir@gmail.com>
2017-01-24 23:23:09 +0100

    Support for connecting to server and reading greeting

--- src/lib.rs
+++ src/lib.rs
@@ -15,11 +15,13 @@ extern crate log;
 #[macro_use]
 extern crate error_chain;
 extern crate openssl;
+extern crate regex;
 
 use std::io::BufReader;
 use openssl::ssl::{SslMethod, SslConnectorBuilder};
 use std::net::TcpStream;
 use std::path::PathBuf;
+use regex::Regex;
 
 pub mod errors {
     error_chain! {
@@ -27,6 +29,8 @@ pub mod errors {
             Io(::std::io::Error);
             SslStack(::openssl::error::ErrorStack);
             SslHandshake(::openssl::ssl::HandshakeError<::std::net::TcpStream>);
+            UTF8Error(::std::string::FromUtf8Error);
+            RegexError(::regex::Error);
         }
     }
 }
@@ -34,8 +38,10 @@ use errors::*;
 
 mod tcpstream;
 mod tcpreader;
+mod pop3result;
 use tcpstream::TCPStreamType;
 use tcpreader::TCPReader;
+use pop3result::POP3Data;
 
 #[derive(PartialEq)]
 #[derive(Debug)]
@@ -76,7 +82,7 @@ impl POP3Connection {
             _ => return Err("Unknown auth type".into()),
         };
 
-        let ctx = POP3Connection {
+        let mut ctx = POP3Connection {
             account: account,
             stream: stream,
             reader: reader,
@@ -85,6 +91,57 @@ impl POP3Connection {
         };
         trace!("Connection Established");
         debug!("POP3State::{:?}", ctx.state);
+        ctx.read_greeting()?;
+        ctx.state = POP3State::AUTHORIZATION;
+        debug!("POP3State::{:?}", ctx.state);
         Ok(ctx)
+    }
+
+    pub fn login(&mut self) {
+        assert!(self.state == POP3State::AUTHORIZATION);
+        trace!("Attempting to Login");
+        let username = &self.account.username.clone();
+        let auth_user_cmd = self.send_command("USER", Some(username)).unwrap();
+    }
+
+    fn read_greeting(&mut self) -> Result<()> {
+        trace!("Reading Greeting from Server");
+        let greeting = &self.read_response(false)?[0];
+        let re = Regex::new(r"(<.*>)\r\n$")?;
+        for cap in re.captures_iter(greeting) {
+            self.timestamp = cap[1].to_string();
+        }
+        Ok(())
+    }
+
+    fn send_command(&mut self, command: &str, param: Option<&str>) -> Result<Vec<String>> {
+        let x = Vec::new();
+        Ok(x)
+    }
+
+    fn read_response(&mut self, is_multiline: bool) -> Result<Vec<String>> {
+        const LF: u8 = 0x0a;
+        let mut response_data: Vec<String> = Vec::new();
+        let mut buff = Vec::new();
+
+        //First read the status line
+        self.reader.read_until(LF, &mut buff)?;
+        response_data.push(String::from_utf8(buff.clone())?);
+        info!("S: {}", response_data[0]);
+
+        let mut complete = false;
+        while !complete && is_multiline {
+            buff.clear();
+            self.reader.read_until(LF, &mut buff)?;
+            let line = String::from_utf8(buff.clone())?;
+            if line == ".\r\n" {
+                complete = true;
+            } else {
+                // Don't add the final .CRLF to the response.
+                // It's useless for us
+                response_data.push(line);
+            }
+        }
+        Ok(response_data)
     }
 }
--- src/tcpstream.rs
+++ src/tcpstream.rs
@@ -5,4 +5,3 @@ pub enum TCPStreamType {
     Plain(TcpStream),
     SSL(SslStream<TcpStream>),
 }
-
